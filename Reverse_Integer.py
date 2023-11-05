684c8bea10a5ec53f413f3c22fdad931f0a44968
SAIGOWTHAM0306 <119730087+SAIGOWTHAM0306@users.noreply.github.com>
2023-11-05 19:17:41 +0530

    def reverse_integer(x):     if x < 0:         sign = -1         x = -x     else:         sign = 1
        # Convert the integer to a string, reverse it, and remove leading zeros
        reversed_str = str(x)[::-1].lstrip('0')
    
        # Convert the reversed string back to an integer
        reversed_int = int(reversed_str)
    
        # Apply the sign to the reversed integer
        reversed_int *= sign
    
        # Check for integer overflow
        if reversed_int < -2**31 or reversed_int > 2**31 - 1:
            return 0
    
        return reversed_int
    
    # Input
    x = int(input())
    
    # Calculate the reverse
    result = reverse_integer(x)
    
    # Output
    print(result)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,11 +1,30 @@
-n=int(input())
-rev=0
-a=abs(n)
-while(a!=0):
-    r=a%10
-    rev=rev*10+r
-    a=a//10
-if(n<0):
-    print(rev*-1)
-else:
-    print(rev)
+def reverse_integer(x):
+    if x < 0:
+        sign = -1
+        x = -x
+    else:
+        sign = 1
+
+    # Convert the integer to a string, reverse it, and remove leading zeros
+    reversed_str = str(x)[::-1].lstrip('0')
+
+    # Convert the reversed string back to an integer
+    reversed_int = int(reversed_str)
+
+    # Apply the sign to the reversed integer
+    reversed_int *= sign
+
+    # Check for integer overflow
+    if reversed_int < -2**31 or reversed_int > 2**31 - 1:
+        return 0
+
+    return reversed_int
+
+# Input
+x = int(input())
+
+# Calculate the reverse
+result = reverse_integer(x)
+
+# Output
+print(result)
